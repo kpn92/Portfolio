6388035d27cc89cb0fc78e40521661142b2246c4
Konstantinos Panousis <145965555+kpn92@users.noreply.github.com>
2025-02-15 11:45:40 +0200

    Update Function-CalcEastern.sql

--- Function-CalcEastern.sql
+++ Function-CalcEastern.sql
@@ -1,36 +1,35 @@
-CREATE FUNCTION	[dbo].[CalcEastern](@pYear int)
-		RETURNS DATETIME
-		AS
-		BEGIN
-			DECLARE	@D	int
-			DECLARE	@E	int
-		
-			
-			SET	@D = (19 * (@pYear % 19) + 16) % 30
-			SET	@E = @D + (2 * (@pYear % 4) + 4 * (@pYear % 7) + 6 * @D) % 7 + 3
-		
-			DECLARE	@RetYear	VARCHAR(4)
-			SET	@RetYear = CONVERT(VARCHAR(4), @pYear)
-		
-			DECLARE	@RetMonth	VARCHAR(2)
-			DECLARE	@RetDay		VARCHAR(2)
-		
-			IF @E > 30
-				BEGIN
-					SET	@RetMonth = '05'
-					SET	@RetDay = CONVERT(VARCHAR(2), @E % 30)
-				END
-			ELSE
-				BEGIN
-					SET	@RetMonth = '04'
-					SET	@RetDay = CONVERT(VARCHAR(2), @E)
-				END
-			
-			IF LEN(@RetDay) = 1
-				SET	@RetDay = '0' + @RetDay
-		
-			RETURN @RetYear + @RetMonth + @RetDay
-		END
+CREATE FUNCTION [dbo].[CalcEastern](@pYear INT)
+RETURNS DATETIME
+AS
+BEGIN
+    DECLARE @D INT
+    DECLARE @E INT
+
+    -- Calculate the date of Easter using the algorithm
+    SET @D = (19 * (@pYear % 19) + 16) % 30
+    SET @E = @D + (2 * (@pYear % 4) + 4 * (@pYear % 7) + 6 * @D) % 7 + 3
+
+    DECLARE @RetYear INT
+    SET @RetYear = @pYear
+
+    DECLARE @RetMonth INT
+    DECLARE @RetDay INT
+
+    -- Determine the month and day based on the calculated value of @E
+    IF @E > 30
+    BEGIN
+        -- If @E is greater than 30, Easter is in May
+        SET @RetMonth = 5
+        SET @RetDay = @E % 30
+    END
+    ELSE
+    BEGIN
+        -- Otherwise, Easter is in April
+        SET @RetMonth = 4
+        SET @RetDay = @E
+    END
+
+    -- Return the calculated date as a DATETIME object
+    RETURN DATEFROMPARTS(@RetYear, @RetMonth, @RetDay)
+END
 GO
-
-
